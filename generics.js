42c2c892d8c37c832615a6d709281bb720dea79d
phpfreak6 <php.freak6@gmail.com>
2022-02-01 15:34:45 +0530

    Update generics.js

--- generics.js
+++ generics.js
@@ -1,5 +1,5 @@
-We can classify generics as if we want to use type checking and make it possible to function to
-receive and handle multiple or specific type of arguments types
+/* We can classify generics as if we want to use type checking and make it possible to function to
+receive and handle multiple or specific type of arguments types */
 
 function sendCombinedArray<A, B>(a: A, b: B): [A, B] {
     return [a, b];
